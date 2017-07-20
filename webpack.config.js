d68043ececa0991e2e4ff04e1855c6b28b3776d4
Oskar Schöldström <m@oxy.fi>
2017-07-20 10:17:00 -0500

    add watch task

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    'wp-gravityforms-timber': path.resolve('./js/main.js')
+    'wp-gravityforms-timber': path.resolve('./js/wp-gravityforms-timber.js'),
   },
-
   output: {
     filename: '[name].js',
-    path: path.resolve('./dist')
+    path: path.resolve('./dist'),
   },
-
-  devtool: '#cheap-source-map',
-
+  devtool: '#source-map',
   externals: {
     jquery: 'jQuery',
   },
-
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'buble-loader', options: { objectAssign: '.merge' } },
+          { loader: 'buble-loader', options: { objectAssign: 'Object.assign' } },
         ]
       }
     ]
@@ -36,7 +32,7 @@ module.exports = {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
-      debug: false,
+      debug: process.argv.indexOf('--watch') === -1,
       stats: { colors: true },
     }),
   ]
